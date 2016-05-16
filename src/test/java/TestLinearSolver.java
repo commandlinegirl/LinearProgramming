04fa9619ffee858389c0a73a0958c2292728047f
Aleksandra Zalcman <biokere@gmail.com>
2016-05-16 19:52:18 +0200

    Correct tests

--- src/test/java/TestLinearSolver.java
+++ src/test/java/TestLinearSolver.java
@@ -1,3 +1,4 @@
+
 import com.commandlinegirl.lp.InputReader;
 import com.commandlinegirl.lp.Objective;
 import com.commandlinegirl.lp.OptimizationType;
@@ -6,9 +7,10 @@ import com.commandlinegirl.lp.exceptions.InfeasibleException;
 import com.commandlinegirl.lp.exceptions.UnboundedException;
 import com.commandlinegirl.lp.solvers.LinearSolver;
 import com.commandlinegirl.lp.solvers.Solver;
-import junit.framework.Assert;
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.net.URL;
 import java.util.List;
 
 public class TestLinearSolver {
@@ -16,15 +18,19 @@ public class TestLinearSolver {
     @Test
     public void testSolve() throws UnboundedException, InfeasibleException {
         InputReader freader = new InputReader();
-        List<List<Double>> input = freader.readInput("/Users/kereish/repos/LinearProgramming/src/test/resources/input01.txt");
+
+        URL url = Thread.currentThread().getContextClassLoader().getResource("input01.txt");
+        Assert.assertNotNull(url);
+
+        List<List<Double>> input = freader.readInput(url.getPath());
 
         double[][] converted = freader.convertDoublesMatrix(input);
         Tableau tableau = new Tableau(converted);
 
         Solver solver = new LinearSolver(OptimizationType.MAX);
         Objective result = solver.solve(tableau);
-        double[] expected = new double[] {0.0, 0.0, 0.19999999999999996, 2.6, 0.8, 0.0, 1.0, 708.0};
-        Assert.assertEquals(expected, result.getSolution());
+        double[] expected = new double[] {708.0, 48.0, 84.0, 0.0, 0.0, 0.0, 60.0, 0.0};
+        Assert.assertArrayEquals(expected, result.getSolution(), Tableau.EPSILON);
 
     }
 }
